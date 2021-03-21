9cc82fc30c89aa06505c69fbd7b472065196bc0c
Nick Dewaele <nick.dewaele@kuleuven.be>
2021-03-21 17:57:10 +0100

    new restoring system with Project & Manifest.toml

--- src/EnvironmentRestore.jl
+++ src/EnvironmentRestore.jl
@@ -1,5 +1,41 @@
 module EnvironmentRestore
 
-# Write your package code here.
+    export archiveEnvironment, restoreEnvironment
+    using Pkg
+
+    function archiveEnvironment(packages :: Pair{String, String}... ;dir=pwd(), force=false)
+        projectFiles = ["Project.toml", "Manifest.toml"]
+        if !force && !isempty(intersect(readdir(dir), projectFiles))
+           print("Directory already contains Julia project files.\nRun with keyword argument ")
+           printstyled("force = true", color=:cyan)
+           println(" to overwrite.")
+           return
+        end
+
+        # Start from a copy of the default environment (normally ~/.julia/environments/vX.Y)
+        env = dirname(Base.active_project())
+        for projFile ∈ projectFiles
+            cp(joinpath(env, projFile), joinpath(dir, projFile); force=true)
+        end
+
+        # Add packages to the new environment
+        try
+            Pkg.activate(dir)
+            for (path, commit) ∈ packages
+                Pkg.add(path=path, rev=commit) 
+            end
+        catch e
+            printstyled(stderr, "\nERROR: ", bold=true, color=:red)
+            println("Modifying the new environment " * dir * " caused the following error: ")
+            throw(e)
+        finally
+            Pkg.activate(env)
+        end
+
+        printstyled(" Successfully archived ", bold=true, color=:green)
+        println("environment in ", joinpath(dir, "Manifest.toml"), ".")
+    end
+
+    restoreEnvironment(dir=pwd()) = Pkg.activate(dir)
 
 end
